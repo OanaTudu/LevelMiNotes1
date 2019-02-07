9e217f77f94d752d39e685468e513f69380e0004
OanaTudu <18587729+OanaTudu@users.noreply.github.com>
2019-02-07 13:57:33 -0800

    added the web version

--- flaskapp/flaskexample/__init__.py
+++ flaskapp/flaskexample/__init__.py
@@ -1,10 +1,11 @@
+
 from flask import Flask
 import os
 from flask import Flask, flash, request, redirect, url_for
 from werkzeug.utils import secure_filename
+
 app = Flask(__name__)
 from flaskexample import views
 
-
 UPLOAD_FOLDER = '/work/insight/flaskapp/uploaded'
-ALLOWED_EXTENSIONS = set(['xml', 'mxl', 'capx', 'abc', 'krn'])
+ALLOWED_EXTENSIONS = set(['xml', 'mxl', 'abc', 'krn'])
--- flaskapp/flaskexample/a_Model.py
+++ flaskapp/flaskexample/a_Model.py
@@ -4,52 +4,50 @@ import json
 import numpy as np
 import pandas as pd
 import re
+import os
 import music21
 from music21 import *
+import matplotlib.pyplot as plt
 from sklearn import linear_model
-from sklearn.model_selection import train_test_split
+from sklearn.model_selection import train_test_split, cross_val_score, GridSearchCV
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.metrics import classification_report, confusion_matrix
 
+
 def ModelIt(fromUser  = 'Default', fileName = []):
   #load the data
-  musicFeat = pd.read_csv('/Users/oana/work/insight/features.csv')
-  labels = pd.read_csv('/Users/oana/work/insight/labels1.csv')
+  musicFeat = pd.read_csv('/home/ubuntu/flaskapp/features6.csv')
+  labels = pd.read_csv('/home/ubuntu/flaskapp/labels3.csv')
 
   #one big dataframe
   df = pd.concat([musicFeat, labels], axis=1)
-  df = df.drop(['Unnamed: 0', 'Tempo'], axis = 1)
-  df = df.drop(['Composer', 'Title', 'KeySignature','Instrument','initialTimeSignature', 'timeSignatureChange'], axis = 1)
-  #print(df.columns)
+  #Drop weird first column, drop the composer and title fields
+  df = df.drop(['Unnamed: 0'], axis = 1)
+  df = df.drop(['Composer', 'Title'], axis = 1)
+
   #elim rows with no labels:
   df = df.loc[df['Labels'] != 0]
-
-  #print(df.info())
+  #define the feature dataframe
   musicFeatures = df.drop(['Labels'], axis =1).values
+  #define the labels series
   lbls = df['Labels'].values
-  #print(lbls)
+
   #reduce labels to 3:
   lbls3=[]
   for i in lbls:
-        if i == 2 or i == 3:
-            x = 1
-        elif i == 4 or i == 5:
-            x = 2
-        elif i == 6 or i == 7 or i==8:
-            x = 3
-        lbls3.append(x)
+      if i == 2 or i == 3:
+          x = 1
+      elif i == 4 or  i == 5:
+          x = 2
+      elif i==8 or i == 6 or i == 7 :
+          x = 3
+      lbls3.append(x)
   lbls3 = pd.Series(lbls3).values
   #define the inputs for the classifier
-  #X_htd = musicFeatures[:,1]
-  y = lbls3.reshape(-1,1)
-  #X_htd = X_htd.reshape(-1,1)
-  #split in test-train sets, classify
-  print(musicFeatures.shape)
-  print(len(lbls3))
-  #raise Exception(lbls3)
-  X_train, X_test, y_train, y_test = train_test_split(musicFeatures, lbls3, test_size = 0.25, random_state = 21, stratify=y)
+  y = lbls.reshape(-1,1)
+  X_train, X_test, y_train, y_test = train_test_split(musicFeatures, lbls, test_size = 0.2, random_state = 21, stratify=y)
 
-  knn = KNeighborsClassifier(n_neighbors = 12)
+  knn = KNeighborsClassifier(n_neighbors = 4)
   knn.fit(X_train, y_train)
   y_pred = knn.predict(X_test)
   pickle.dump(knn, open('model.pkl','wb'))
